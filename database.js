4af5c0ed95bed6adf7ae762e7ce864e6fa2f98b0
rkddusen <k99endus@naver.com>
2022-06-03 13:17:07 +0900

    add nodejs

---
+++ database.js
@@ -0,0 +1,27 @@
+const mysql = require('mysql');
+
+module.exports = function () {
+    return {
+        init: function () {
+            return mysql.createConnection({
+                host: '127.0.0.1',
+                port: '3306',
+                user: 'root',
+                password: 'eeldhd4120',
+                database: 'movie',
+                dateStrings: 'date',
+                multipleStatements : true//다중쿼리문 보낼수있음
+            })
+        },
+        
+        db_open: function (con) {
+            con.connect(function (err) {
+                if (err) {
+                    console.error('mysql connection error :' + err);
+                } else {
+                    console.info('mysql is connected successfully.');
+                }
+            })
+        }
+    }
+};
