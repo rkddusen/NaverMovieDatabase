81922965002fa9cffc905a291770cad6fd8859d6
rkddusen <k99endus@naver.com>
2022-06-09 18:32:13 +0900

    change index file

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ server.set('port', process.env.PORT || 8080);
 server.set('hostname', '127.0.0.1');
 
 //정적(css,일부js,사진)파일을 사용 가능하게끔
-server.use(express.static(__dirname + "/public"));
+server.use(express.static(__dirname));
 server.use(bodyParser.urlencoded({ extended: false }));
 server.use(bodyParser.json());
 
@@ -33,35 +33,58 @@ server.get("/", (req, res) => {
 
     res.sendFile(__dirname + "/index.html");
 });
-let search='';
+let search = '';
 const index = fs.readFileSync('./index.ejs', 'utf8');
 server.post("/title", (req, res) => {
     search = req.body.text;
-    let id = '';
-    let title = '';
-    let movie_rate = '';
-    let netizen_rate = '';
-    let netizen_count = '';
-    let journalist_score = '';
-    let scope = '';
-    let playing_time = '';
-    let sql='SELECT * FROM movie where title LIKE "%'+search+'%";';
-    let sqll='SELECT COUNT(*) AS number FROM movie where title like "%'+search+'%";';
-    connection.query(sql+sqll,
+    let sql = 'SELECT * FROM movie where title LIKE "%' + search + '%";';
+    let sqll = 'SELECT COUNT(*) AS number FROM movie where title like "%' + search + '%";';
+    connection.query(sql + sqll,
         function (error, rows, fields) {
             if (error) {
                 console.log(error);
             }
             else {
-                let dataResult=rows[0];
-                let countResult=rows[1];
+                let dataResult = rows[0];
+                let countResult = rows[1];
                 let number = countResult[0].number;
-                for (var i in rows[0]) {
-                    title += dataResult[i].title+"/"+dataResult[i].id+"//"
 
+                //검색 결과 화면에 영화제목, 관람객 평점, 관람객 수, 네티즌 평점, 네티즌 수, 평론가 평점, 평론가 수,
+                //상영시간, 개봉일을 담을 예정
+                let id = new Array();
+                let title = new Array();
+                let movie_rate = new Array();
+                let netizen_rate = new Array();
+                let netizen_count = new Array();
+                let journalist_score = new Array();
+                let journalist_count = new Array();
+                let playing_time = new Array();
+                let opening_date = new Array();
+                let image = new Array();
+                for (var i in rows[0]) {
+                    id[i] = dataResult[i].id
+                    title[i] = dataResult[i].title
+                    movie_rate[i] = dataResult[i].movie_rate
+                    netizen_rate[i] = dataResult[i].netizen_rate?dataResult[i].netizen_rate:0
+                    netizen_count[i] = dataResult[i].netizen_count?dataResult[i].netizen_count:0
+                    journalist_score[i] = dataResult[i].journalist_score?dataResult[i].journalist_score:0
+                    journalist_count[i] = dataResult[i].journalist_count?dataResult[i].journalist_count:0
+                    playing_time[i] = dataResult[i].playing_time
+                    opening_date[i] = dataResult[i].opening_date
+                    image[i] = dataResult[i].image
                 }//데이터 생성
                 var page = ejs.render(index, {
-                    title: title
+                    title: title,
+                    id:id,
+                    movie_rate:movie_rate,
+                    netizen_rate:netizen_rate,
+                    netizen_count:netizen_count,
+                    journalist_score:journalist_score,
+                    journalist_count:journalist_count,
+                    playing_time:playing_time,
+                    opening_date:opening_date,
+                    image:image,
+                    number:number
                 });
                 //응답
                 console.log(title);
